3094b3d1e0864e92ad26e3cd46f92ab466d1b4ff
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-20 19:43:31 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    function disableTextS() {
+        $('body').css('-webkit-user-select','none');
+        $('body').css('-moz-user-select','none');
+        $('body').css('-ms-user-select','none');
+        $('body').css('-o-user-select','none');
+        $('body').css('user-select','none');
+    }
+    
+    disableTextS();
+})
