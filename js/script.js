e958a07105b48aa841b23713619eeae4196d4896
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-22 17:41:57 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    disableTextS();
+
     function disableTextS() {
         $('body').css('-webkit-user-select','none');
         $('body').css('-moz-user-select','none');
@@ -7,6 +9,16 @@ $(function() {
         $('body').css('-o-user-select','none');
         $('body').css('user-select','none');
     }
+
+    scrollPage(); 
+
+    function scrollPage() {
+        $('nav a').click(function(){
+            var href = $(this).attr('href');
+            var offSetTop = $(href).offset().top;
     
-    disableTextS();
+            $('html, body').animate({'scrollTop':offSetTop});
+            return false;
+        });
+    }
 })
